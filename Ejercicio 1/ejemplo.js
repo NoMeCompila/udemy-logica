41b349e2206f9207e1e89876d4f869dba5a3860e
CabaFer1 <cabafer1@gmail.com>
2024-01-18 08:04:21 -0300

    Delete Ejercicio 1 directory

--- Ejercicio 1/ejemplo.js
+++
@@ -1,9 +0,0 @@
-var nombre;
-
-console.log("ingrese nombre: ")
-
-process.stdin.on('data', function(data){
-    nombre = data.toString().trim();
-    console.log(`Hola ${nombre}! \n`);
-    process.exit();
-});
